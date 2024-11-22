0f611892d7060dc7cc18e13bb0e49320d1b081ce
Maria Orlina <mariaorlina@Marias-MacBook-Air.local>
2024-11-22 04:47:57 -0600

    added favicon to public folder

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['https://raw.githubusercontent.com/MariaOrlina/portfolio-image/refs/heads/main/IMG_3041.jpeg'], // Add your allowed domain here
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
